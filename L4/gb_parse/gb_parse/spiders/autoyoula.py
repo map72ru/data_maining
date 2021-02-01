3ab8afb421bb141d4bcec77a9557a92d2ad68dcf
Andre <upgoto@gmail.com>
2021-02-02 00:11:29 +0500

    L4+

--- L4/gb_parse/gb_parse/spiders/autoyoula.py
+++ L4/gb_parse/gb_parse/spiders/autoyoula.py
@@ -1,7 +1,9 @@
+import pymongo
 import scrapy
 import re
 from urllib.parse import unquote
 from base64 import b64decode
+from json import loads
 
 def get_characteristics(resp):
     values = resp.xpath(
@@ -9,6 +11,13 @@ def get_characteristics(resp):
     titles = resp.xpath(
         "//div[@class='AdvertSpecs_row__ljPcX']/div[@class='AdvertSpecs_label__2JHnS']/text()").extract()
     return dict(zip(titles, values))
+
+def get_characteristics_items(elm) -> list:
+    items = []
+    for itm in elm[1]:
+        items.append(dict(zip(itm[1][::2], itm[1][1::2])))
+    return items
+
 
 class AutoyoulaSpider(scrapy.Spider):
     name = "autoyoula"
@@ -25,8 +34,13 @@ class AutoyoulaSpider(scrapy.Spider):
         "title": lambda resp: resp.css("div.AdvertCard_advertTitle__1S1Ak::text").get(),
         "price": lambda resp: float(resp.css('div.AdvertCard_price__3dDCr::text').get().replace("\u2009", '')),
         "description": lambda resp: resp.css("div.AdvertCard_descriptionInner__KnuRi::text").get(),
-        "characteristics": get_characteristics,
+        "main_characteristics": get_characteristics,
     }
+
+    def __init__(self, user, passwd, **kwargs):
+        super().__init__(**kwargs)
+        self.database = pymongo.MongoClient(f'mongodb+srv://{user}:{passwd}@mdbcluster.cg0fo.mongodb.net/?retryWrites=true&w=majority')[
+            'gb_parse_12']
 
     def parse(self, response, **kwargs):
         brands_links = response.css(self.css_query["brands"])
@@ -56,19 +70,51 @@ class AutoyoulaSpider(scrapy.Spider):
         m = re.search('window.transitState = decodeURIComponent\("(.+?)"\);', text)
         text = unquote(m.group(1))
         m = re.search('"phone","(.+?)"', text)
+        # телефон
         if m is not None:
-            phone = b64decode(m.group(1))
-            data['phone'] = phone
+            data['phone'] = "".join([chr(b) for b in b64decode(b64decode(m.group(1)))])
         m = re.search('"youlaId","(.+?)"', text)
         if m is not None:
             data['author_url'] = 'https://youla.ru/user/' + m.group(1)
-
-
-
-
+        # Картинки
+        data['images'] = self.get_images(text)
+        # Все характеристики
+        data['add_characteristics'] = self.get_add_characteristics(text)
+        # Сохранение в БД
+        collection = self.database['yuola']
+        collection.insert_one(data)
 
 
     @staticmethod
     def gen_task(response, link_list, callback):
         for link in link_list:
             yield response.follow(link.attrib["href"], callback=callback)
+
+    @staticmethod
+    def get_images(text: str) -> list:
+        images = []
+        data = re.findall('\[\"\^0",\[\"type\",\"photo\",(.+?)]],', text)
+        for item in data:
+            key = None
+            image_set = {}
+            for elm in item.split(','):
+                if (elm == '"big"' or elm == '"medium"' or elm == '"large"' or elm == '"small"'):
+                    key = elm[1:len(elm)-1]
+                    continue
+                if key is not None:
+                    image_set[key] = elm
+                    key = None
+            images.append(image_set)
+        return images
+
+    @staticmethod
+    def get_add_characteristics(text) -> dict:
+        item = {}
+        m = re.search(',"techStruct",\["\^1",(.+?)]]]],"has3d",', text)
+        if m is not None:
+            text = m.group(1)
+            data = loads(text + ']]]')
+            for elm in data:
+                item[elm[1][1]] = get_characteristics_items(elm[1][3])
+        return item
+
--- L4/gb_parse/main.py
+++ L4/gb_parse/main.py
@@ -1,10 +1,13 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from L4.gb_parse.gb_parse.spiders.autoyoula import AutoyoulaSpider
+import os
+from dotenv import load_dotenv
 
 if __name__ == '__main__':
+    load_dotenv()
     crawler_settings = Settings()
     crawler_settings.setmodule('gb_parse.settings')
     crawler_process = CrawlerProcess(settings=crawler_settings)
-    crawler_process.crawl(AutoyoulaSpider)
+    crawler_process.crawl(AutoyoulaSpider, os.getenv('USER'), os.getenv('PASSWD'))
     crawler_process.start()
